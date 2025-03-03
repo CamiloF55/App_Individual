e96a633cc429351430692514ccaa6d11eec2d370
Tu Nombre <tuemail@example.com>
2025-03-02 23:51:13 -0500

    V. 1.1

--- app/src/main/java/com/example/formulario/FormActivity2.kt
+++ app/src/main/java/com/example/formulario/FormActivity2.kt
@@ -1,6 +1,8 @@
 package com.example.formulario
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,16 @@ class FormActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_form2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+            val Form_Form = findViewById<Button>(R.id.form_check)
+        Form_Form.setOnClickListener{
+            FormToCheck()
         }
+
+    }
+    private fun FormToCheck() {
+        val o = Intent (this, CheckActivity2::class.java)
+        startActivity(o)
     }
 }
--- app/src/main/java/com/example/formulario/MainActivity.kt
+++ app/src/main/java/com/example/formulario/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.formulario
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+    val MainForm = findViewById<Button>(R.id.main_form)
+    MainForm.setOnClickListener{
+        mainToForm()
+    }
+}
+private fun mainToForm() {
+    val i = Intent(this, FormActivity2::class.java)
+    startActivity(i)
     }
 }
